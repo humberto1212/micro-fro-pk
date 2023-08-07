28325386e33d88a4602df159cff1b8f1a5ee272b
Humberto <angeluccirincon@gmail.com>
2023-08-07 16:47:41 +0200

    Rename App

---
+++ pk-home/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+import Button from 'pklist/Button'
+
+function App() {
+
+
+  return (
+    <>
+
+      <h1>Host Component</h1>
+      <Button/>
+
+    </>
+  )
+}
+
+export default App
