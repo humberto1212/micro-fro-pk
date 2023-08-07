b891e2c38fa0e939fddc9b32d18f3357b3b3d698
Humberto <angeluccirincon@gmail.com>
2023-08-07 16:46:36 +0200

    Use federation plugin, Add build config

--- pk-home/vite.config.ts
+++ pk-home/vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from '@originjs/vite-plugin-federation'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: 'remotecomponent1',
+      remotes: [
+        {
+          pklist: 'http://localhost:5173/assets/remoteEntry.js'
+          // sharedComp: {
+          //   external: `Promise.resolve(window.remoteURL)`,
+          //   from: 'vite',
+          //   externalType: 'promise',
+          // }
+        }
+      ],
+      shared: ['react', 'react-dom']
+    })
+  ],
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false
+  }
 })
--- pk-list/vite.config.ts
+++ pk-list/vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from '@originjs/vite-plugin-federation'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+      react(),
+      federation({
+           name: 'remotecomponent1',
+           filename: 'remoteEntry.js',
+           exposes: {
+               './Button': './src/components/buttons.tsx'
+           },
+           shared: ['react', 'react-dom'],
+      }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false
+  }
+});
+
